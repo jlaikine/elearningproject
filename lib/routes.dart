af5032c4a588520c6a209fc6906771f84e440fe5
Johan <laikinejohan@gmail.com>
2023-04-16 16:20:49 +0200

    add routes and pages

---
+++ lib/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:elearningproject/screens/home.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  Home.routeName: (context) => Home(),
+};
